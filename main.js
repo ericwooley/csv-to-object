fa1cbe204102bccce90022cea3309e07e72ff318
Eric Wooley <ericwooley@gmail.com>
2014-05-13 14:15:49 -0700

    Fixed regex

--- main.js
+++ main.js
@@ -7,32 +7,50 @@ function svToObject(settings){
 		filename: '',
 		string: ''
 	});
+
 	settings.dilemeter = escapeForRegex(settings.dilemeter);
 	settings.textQualifier = escapeForRegex(settings.textQualifier);
 	var d = settings.dilemeter;
 	var tq = settings.textQualifier;
-	// This is the ultimate split, which would countain a dilimeter wrapped in text qualifiers
+
+	///////////////////////////////////////////////////////////////
+	/// This appears to be glitched
+	///////////////////////////////////////////////////////////////
 	var searchArray = [
-		tq+d+tq, // First case to search for, eg: ","
-		tq+d, // Second case to search for, eg: ",
-		d+tq, // Third case to search for, eg: ,"
-		d, // Last case to search for, eg: ,
-		// '\\n'
+		"(?:"+tq+d+tq+")", // First case to search for, eg: ","
+		"(?:"+tq+d+")", // Second case to search for, eg: ",
+		"(?:"+d+tq+")", // Third case to search for, eg: ,"
+		"(?:"+d+")", // Last case to search for, eg: ,
+		"(?:"+tq+"$)", // if the text qualifier is the very last thing
 	];
-	var regexString = "("+searchArray.join('|')+')';
+	var regexString = "(?:"+searchArray.join('|')+')';
+	console.log(regexString)
 	var regex = new RegExp(regexString);
+
+
 	if(settings.filename.length > 0)
 		settings.string = readFile(settings.filename);
 	var splitContents = settings.string.split('\n');
 	var parsedContents = [];
 	for (var i = 0; i < splitContents.length; i++) {
 		// console.log("");
-		// console.log(splitContents[i]);
+		console.log(splitContents[i]);
 		parsedContents.push(splitContents[i].split(regex));
-
 	};
-	console.log(parsedContents);
+	// console.log(parsedContents[1])
 
+	var headers = parsedContents[0];
+	var returnArray = [];
+	for (var i = 1; i < parsedContents.length; i++) {
+		var line = parsedContents[i];
+		var lineObject = {};
+		for (var j = 0; j < line.length; j++) {
+			if(typeof headers[j] === 'string' && headers[j].length > 0)
+				lineObject[headers[j]] = line[j];
+		};
+		returnArray.push(lineObject);
+	};
+	return returnArray;
 }
 function escapeForRegex(str){
 	 return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
@@ -40,24 +58,36 @@ function escapeForRegex(str){
 function readFile(filename){
 	return fs.readFileSync('./scripts/'+filename);
 }
+function processString(string, d, tq){
+
+
+	
+	return string.split(regex);
+
+	// The simpleton way
+	// var splitArray = string.split(tq+d+tq);
+	// var str = string.split(tq+d+tq); // First case to search for, eg: ","
+	// 	str = str.split(tq+d); // Second case to search for, eg: ",
+	// 	str = str.split(d+tq); // Third case to search for, eg: ,"
+	// 	str = str.split(d); // Last case to search for, eg: ,
+	// 	str = str.split(tq); // if the text qualifier is the very last thing
+
+	// return str;
+}
 
 module.exports = svToObject;
+
+
 if(require.main === module) {
 	var testString = [
-"ProductName| ProductCode|ProductDescription| PricingLine| ProductFamily| PartCatagory| ProductLine| UnitOfMeasure| RecommendedSpare| RMAFORMViewable (yes/no)| NorthAmericanStandardWarranty|GlobalStandardWarranty|TotalWarranty| ExtWarrantyEldigable (true/false)| WarrantCatagory| AdvanceExchangeElidgable (true/false)|CapElidgable(true/false)|Active(true/false)|quotable (true/false)|accessableBy|DomesticPrice|InternationalPrice|standardCost",
-"100-01296|Aztek RT Install Kit|Aztek RT Install Kit|Third Party|Third Party|Aztek|Gateway|Each|0|False|None|None|None|False|None|False|False|True|False|All|0|0|0||No",
-"100-03647|E3-48 100Pr CAT5 Cable with Intgratd/ RMT PWR 50'|E3-48 100Pr CAT5, 4RJ21->Unterm, 50ft, dead pr provides rmt pwr, above Grade|E3|E-Series|Cables|E-Series EM|Each|0|False|90 Days|None|90 Days|False|None|False|False|True|False|All|0|0|0||Yes",
-"100-01497|ODC PON Splitter Kit Installation Guide|ODC PON Splitter Kit Installation Guide|C7|C-Series|Documentation|C7-Other|Each|0|False|None|1 Year|4 Years|False|None|False|False|True|False|All|0|0|0||No",
-"100-03521|\"Pre-Assm E3-48 AC MS2 Abv grade/5' Cabl Clped\"|\"Pre-Assm E3-48 AC Powered Unit with MS2 Connectors-Above Grade 5ft cabling w/Coolped mount\"|E3|\"E-Series\"|DSL Units|E3|Each|0|False|None|None|None|False|None|False|False|True|False|All|0|0|0||No",
-"100-03522|Pre-Assm E3-48 AC 710 Abv grade/5' Cabl Clped|Pre-Assm E3-48 AC Powered Unit with 710 Connectors-Above Grade 5ft cabling w/Coolped mount|E3|E-Series|DSL Units|E3|Each|0|False|None|None|None|False|None|False|False|True|False|All|0|0|0||No",
-"100-03523|Pre-Assm E3-48 AC MS2 Abv grade/20' Cabl|Pre-Assm E3-48 AC Powered Unit with MS2 Connectors-Above Grade 20ft cabling|E3|E-Series|DSL Units|E3|Each|0|False|None|None|None|False|None|False|False|True|False|All|0|0|0||No",
-"100-03524|Pre-Assm E3-48 AC 710 Abv grade/20' Cabl|Pre-Assm E3-48 AC Powered Unit with 710 Connectors-Above Grade 20ft cabling|E3|E-Series|DSL Units|E3|Each|0|False|None|None|None|False|None|False|False|True|False|All|0|0|0||No",
-"100-03525|Pre-Assm E3-48 LP MS2 Abv grade/5' Cabl Clped|Pre-Assm E3-48 Line Powered Unit with MS2 Connectors - Above Grade 5 ft cabling w/Coolped mount|E3|E-Series|DSL Units|E3|Each|0|False|None|None|None|False|None|False|False|True|False|All|0|0|0||No"];
-svToObject({
-	dilemeter: '|',
-	textQualifier: "\"",
-	string: testString.join('\n')
-});
+		'h1|h2|h3|h4', // The first line will be the headers
+		'value 1|"Value 2"|value 3|"value - 4"'// This is the first row of data
+	];
+	console.log(svToObject({
+		dilemeter: '|',
+		textQualifier: '"',
+		string: testString.join('\n')
+	}));
 } else {
 	console.log("required as a module"); 
 }
